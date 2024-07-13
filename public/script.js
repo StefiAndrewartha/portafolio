c5c88bf8e83f36f62c7bb3d008d64ccfc70aa7e8
Stefi Andrewartha <stefi.andrewartha>
2024-07-13 17:19:11 -0400

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para servir el archivo HTML principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});
