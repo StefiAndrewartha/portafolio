6e13136f4cb6ab74a9ebbcb8344a3aead1c4a689
Stefi Andrewartha <stefi.andrewartha>
2024-07-13 17:46:28 -0400

    Agregar proyectos 'to do list', 'typer-d' y 'colorgame' al portafolio

--- public/script.js
+++ public/script.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
-
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta para servir el archivo HTML principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensaje enviado. ¡Gracias por contactarme!');
 });
